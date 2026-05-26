ce6b86db932a3cd6fee5030ee2b659e99196084e
EnzoCarrilho <enzocarrilho25@gmail.com>
2026-05-26 11:19:12 -0300

    feat: Componetizando header do perfil

--- app/src/main/java/com/example/espectra/ui/screens/TelaPerfilFamiliar.kt
+++ app/src/main/java/com/example/espectra/ui/screens/TelaPerfilFamiliar.kt
@@ -1,18 +1,25 @@
 package com.example.espectra.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.espectra.ui.components.EspectraHeaderAzul
-import com.example.espectra.ui.components.HeaderPerfil
+import com.example.espectra.ui.components.telasPerfil.HeaderPerfil
 
 @Composable
-fun TelaPerfilFamiliar(modifier: Modifier = Modifier) {
+fun TelaPerfilFamiliar(paddingValues: PaddingValues) {
     Column(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        HeaderPerfil()
+        HeaderPerfil(null)
+
+        Column(
+            modifier = Modifier
+        ) {
+            Text("Nome Paciente")
+        }
     }
 }
