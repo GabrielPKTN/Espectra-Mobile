d48aada63aac21349c22946dcc2d893cc2162c64
ALINE ALVES DE SOUZA <25132561@dsn127m1242314.cfp127aluno.local>
2026-06-01 11:26:35 -0300

    feat: modicicado EspectraApiService

--- app/src/main/java/com/example/espectra/service/EspectraApiService.kt
+++ app/src/main/java/com/example/espectra/service/EspectraApiService.kt
@@ -3,13 +3,19 @@ package com.example.espectra.service
 import com.example.espectra.model.*
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Headers
 
 
 interface EspectraApiService {
-    @POST("auth/login") // Ajuste a rota para a correspondente do seu backend
-    suspend fun login(@Body request: DataTelaLogin): Response<RespostaAutenticacao>
 
-    @POST("auth/cadastro") // Ajuste a rota para a correspondente do seu backend
-    suspend fun cadastrar(@Body request: DataTelaCadastro): Response<RespostaAutenticacao>
+
+    @Headers("Content-Type: application/json")
+
+
+    @POST("v1/espectra/usuario/login")
+    suspend fun login(
+        @Body request: DataTelaLogin
+    ): Response<RespostaAutenticacao>
 }
