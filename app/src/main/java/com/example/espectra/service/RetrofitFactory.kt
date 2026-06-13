9ba1a9288285c52cb26570d78daa25af106d801d
EnzoCarrilho <enzocarrilho25@gmail.com>
2026-06-12 23:19:47 -0300

    fix: Ajusta a base url para o render

--- app/src/main/java/com/example/espectra/service/RetrofitFactory.kt
+++ app/src/main/java/com/example/espectra/service/RetrofitFactory.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitFactory {
 
-    val BASE_URL = "http://10.0.2.2:8080/"
+    val BASE_URL = "https://espectra-back.onrender.com/"
 
     private val retrofitFactory = Retrofit
         .Builder()
--- app/src/main/java/com/example/espectra/service/RetrofitInstance.kt
+++ app/src/main/java/com/example/espectra/service/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
 
 
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "https://espectra-back.onrender.com/"
 
     val espectraApiService: EspectraApiService by lazy {
         Retrofit.Builder()
